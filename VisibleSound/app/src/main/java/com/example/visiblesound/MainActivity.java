890eadc07098ae266ddc693b10067925740e6b86
Dong-wook94 <gygacpu@naver.com>
2019-08-05 18:00:39 +0900

    VisibleSound application 프로젝트 생성

---
+++ VisibleSound/app/src/main/java/com/example/visiblesound/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.visiblesound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
