db90073b5e3d343c0875d795a73544086a1c99a2
YunSeoWon <floidea@naver.com>
2019-08-07 16:19:57 +0900

    파일 전송 요청하는 클라이언트 예제 코드 추가

---
+++ Seowon/EncodingClient/src/main/java/ClientMain.java
@@ -0,0 +1,40 @@
+
+import okhttp3.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ClientMain {
+    static String url = "http://localhost:8080/upload";
+    static String filename = "src/main/resources/sample.m4a";
+    public static void main(String[] args) {
+        File file = new File(filename);
+
+        send2Server(file);
+
+    }
+
+    static void send2Server(File file) {
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("uploadFile", file.getName(), RequestBody.create(MultipartBody.FORM, file))
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(Call call, Response response) throws IOException
+            {
+                System.out.printf("TEST : " , response.body().string());
+            }
+        });
+    }
+}
