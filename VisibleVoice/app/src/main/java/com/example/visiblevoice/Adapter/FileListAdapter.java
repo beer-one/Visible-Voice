b68c4c3b51260a3d70a363fa3c779f572654d174
unknown <thddudwn1012@naver.com>
2019-09-13 01:30:34 +0900

    add adapter package

--- VisibleVoice/app/src/main/java/com/example/visiblevoice/Adapter/FileListAdapter.java
+++ VisibleVoice/app/src/main/java/com/example/visiblevoice/Adapter/FileListAdapter.java
@@ -1,7 +1,9 @@
-package com.example.visiblevoice;
+package com.example.visiblevoice.Adapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.visiblevoice.R;
 
 public class FileListAdapter extends AppCompatActivity {
 
